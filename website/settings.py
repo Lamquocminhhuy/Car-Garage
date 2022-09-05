f776bdb98fa4d48e4930dd89bd20d67344d26ed5
Lam Quoc Minh Huy <lamquocminhhuy@gmail.com>
2022-09-06 02:37:49 +0700

    fix link html

--- website/settings.py
+++ website/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ylucjkw-nxf1$n_5u)6n&^!h&)o8g@_^6@qe1j#2%bqdbbzp@)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
