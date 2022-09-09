6f40f67b7e4527fa1b1d77aed944cf83159a4475
Lam Quoc Minh Huy <lamquocminhhuy@gmail.com>
2022-09-10 00:02:20 +0700

    update translate register & log in form to vietnamese

--- garage/forms.py
+++ garage/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
 from .models import Booking, TimeSlot, BookingNoRegistration
 
 class DateInput(forms.DateInput):
@@ -36,7 +37,7 @@ class BookingNoRegistrationForm(forms.ModelForm):
         fields = ["user","service", "date", "time_slot", "phone_number", "note"]
         widgets = {
             'date': DateInput(),
-            'note': forms.Textarea(attrs={'rows': 4})
+            'note': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Ghi chú nếu quý khách có thêm yêu cầu khác.'})
         }   
         labels  = {
             "service": "Dịch vụ",
@@ -44,17 +45,36 @@ class BookingNoRegistrationForm(forms.ModelForm):
             "time_slot": "Thời gian",
             "note": "Ghi chú",
             "phone_number": "Số điện thoại"
-
         }
     def __init__(self, user=None, **kwargs):
         super(BookingNoRegistrationForm, self).__init__(**kwargs)
         self.fields['time_slot'].queryset = TimeSlot.objects.filter(isBusy=False)
 class UserCreationForm(UserCreationForm):
+
     email = forms.EmailField(required=True, label='Email')
+    password1 = forms.CharField(
+        label='Mật khẩu',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+  
+    )
+    password2 = forms.CharField(
+        label='Xác nhận',
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        strip=False,
+     
+    )
+
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ["username", "email", "password1", "password2"]
+
+        labels  = {
+            "username": "Tên đăng nhập",
+            "email": "Email",
+        }
+
       
 
     def save(self, commit=True):
@@ -63,3 +83,4 @@ class UserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
